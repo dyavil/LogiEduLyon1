e79351135ef58179f8ea597c055ed33516187d47
Dyavil <dyavil.q@gmail.com>
2017-03-27 13:28:40 +0200

    test version of coding type exercicev0.2

--- src/main/java/univlyon1/fr/logiedu/View/ExerciceWithSourcesView.java
+++ src/main/java/univlyon1/fr/logiedu/View/ExerciceWithSourcesView.java
@@ -5,6 +5,17 @@
  */
 package univlyon1.fr.logiedu.View;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
+import javafx.concurrent.Worker.State;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -14,6 +25,9 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.web.WebView;
+import netscape.javascript.JSObject;
+import org.apache.commons.io.IOUtils;
 
 /**
  *
@@ -25,6 +39,7 @@ public class ExerciceWithSourcesView extends ExerciceView {
     private GridPane codePane;
     private Label textContent;
     private TextArea sourceContent;
+    private WebView sourceContentWV;
     private Label stdOutput;
     private Label errOutput;
     private Button runButton;
@@ -50,6 +65,39 @@ public class ExerciceWithSourcesView extends ExerciceView {
         this.textContent.getStyleClass().add("exercice-content");
         this.sourceContent = new TextArea(fileContent);
         this.sourceContent.setWrapText(true);
+        this.sourceContentWV = new WebView();
+        this.sourceContentWV.getStylesheets().add("/lib/codemirror.css");
+        
+        this.sourceContentWV.getEngine().loadContent("<html><body style='max-width:200px; height:200px; margin:0px; padding:0px;' ><div style='position: absolute;\n" +
+"        top: 0;\n" +
+"        right: 0;\n" +
+"        bottom: 0;\n" +
+"        left: 0;' id='code'>"+fileContent+"</div></body></html>");
+        this.sourceContentWV.applyCss();
+        this.sourceContentWV.getEngine().getLoadWorker().stateProperty().addListener(new ChangeListener<State>() {
+            public void changed(ObservableValue ov, State oldState, State newState) {
+                if (newState == Worker.State.SUCCEEDED) {
+                    getSourceContentWV().getEngine().executeScript(getJS()+"\n" +
+                    "ace.require(\"ace/ext/language_tools\");var editor = ace.edit(\"code\");editor.setTheme(\"ace/theme/merbivore\");editor.getSession().setMode(\"ace/mode/java\");var cod = editor.getValue();"+"$(function () {\n" +
+"    $('*').click(function (event) {\n" +
+"        event.preventDefault();\n" +
+"        event.stopPropagation();\n" +
+"        clickController.printId(this);\n" +
+"    });\n" +
+"});");
+                        }
+                    }
+                });
+            this.sourceContentWV.getEngine().getLoadWorker().stateProperty().addListener(new ChangeListener<State>() {
+              @Override
+              public void changed(ObservableValue<? extends State> ov, State oldState, State newState) {
+                  if (newState == State.SUCCEEDED) {
+                      JSObject window = (JSObject) sourceContentWV.getEngine().executeScript("window");
+                      window.setMember("clickController", new WebController());
+                    }
+                }
+            }
+          );
         this.runButton = new Button("Run");
         ColumnConstraints midCol = new ColumnConstraints((parentWidth-80)/2);
         this.codePane.getColumnConstraints().add(midCol);
@@ -61,7 +109,7 @@ public class ExerciceWithSourcesView extends ExerciceView {
         this.returnPane.getColumnConstraints().add(amidCol);
         this.getMiddlePane().getRowConstraints().clear();
         this.getMiddlePane().getRowConstraints().add(new RowConstraints(250));
-        this.codePane.add(this.sourceContent, 0, 0);
+        this.codePane.add(this.sourceContentWV, 0, 0);
         this.codePane.add(this.textContent, 1, 0);
         
         this.getMiddlePane().getRowConstraints().add(new RowConstraints(100));
@@ -72,6 +120,21 @@ public class ExerciceWithSourcesView extends ExerciceView {
         this.getMiddlePane().getChildren().clear();
         this.getMiddlePane().add(codePane, 0, 0);
         this.getMiddlePane().add(returnPane, 0, 1);
+    }
+    
+    public String getJS(){
+        try {
+            InputStream f = getClass().getResourceAsStream("/lib/jquery-3.2.0.min.js");
+            String content0 = IOUtils.toString(f, "UTF-8");
+            InputStream f2 = getClass().getResourceAsStream("/styles/ace.js");
+            String content1 = IOUtils.toString(f2, "UTF-8");
+            InputStream f3 = getClass().getResourceAsStream("/styles/ext-language_tools.js");
+            String content2 = IOUtils.toString(f3, "UTF-8");
+            return (content0+content1+content2);
+        } catch (IOException ex) {
+            Logger.getLogger(ExerciceWithSourcesView.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
 
     /**
@@ -108,5 +171,22 @@ public class ExerciceWithSourcesView extends ExerciceView {
     public Button getRunButton() {
         return runButton;
     }
+
+    /**
+     * @return the sourceContentWV
+     */
+    public WebView getSourceContentWV() {
+        return sourceContentWV;
+    }
+    
+    public class WebController {
+    public void printId(Object object) {
+        if (org.w3c.dom.html.HTMLElement.class.isAssignableFrom(object.getClass())) {
+            org.w3c.dom.html.HTMLElement it = (org.w3c.dom.html.HTMLElement) object;
+            System.out.println("Id is " + it.getId() + it.getTextContent().replaceAll("(    )+", "\n"));
+            sourceContent.setText(it.getTextContent().replaceAll("(    )+", "\n"));
+        }
+    }
+}
     
 }
